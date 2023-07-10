3f92fc356e314282ca9169102f497892cf5fdf60
MOM-ISTD <mom-istd@Raphaels-MacBook-Pro.local>
2023-07-10 17:26:44 +0800

    Added db + s3

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { s3 } from '@/lib/s3';
+
 export default async function Home() {
-  return <div className="container mx-auto">Hello world</div>;
+  const result = await s3
+    .getObject({
+      Bucket: process.env.AWS_S3_BUCKET_NAME!,
+      Key: 'favicon.ico',
+    })
+    .promise();
+
+  const body = (result.Body as Buffer).toString('base64');
+  return (
+    <div className="container mx-auto">
+      <img src={`data:image/*;base64,${body}`} />
+    </div>
+  );
 }
