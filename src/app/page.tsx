4a66a4801fad03031f83981b6eb00a47769ac306
MOM-ISTD <mom-istd@Raphaels-MacBook-Pro.local>
2023-07-13 11:43:14 +0800

    Test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import { useSession } from 'next-auth/react';
 export default function Home() {
   const session = useSession();
 
-  console.log(session);
-
   return (
     <div className="container mx-auto">
       <h1 className="text-4xl font-bold">Hello World</h1>
