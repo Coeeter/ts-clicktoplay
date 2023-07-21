de8db84ed30f0b400126fd59a97a01ae4d4fee2d
Noorullah Nasrullah <nasrullah01n@gmail.com>
2023-07-21 16:37:06 +0800

    Added SongItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const songs = await prisma.song.findMany();
 
   return (
-    <div className="">
+    <div className="flex flex-wrap gap-3 justify-center">
       {songs.map(song => (
         <SongItem key={song.id} song={song} />
       ))}
