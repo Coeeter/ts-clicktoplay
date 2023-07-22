d012273fa4e3c2fd4f6d4d5428ca41d811ff3595
Nasrullah <nasrullah01n@gmail.com>
2023-07-23 02:20:16 +0800

    Moved api logic

---
+++ src/lib/songs.ts
@@ -0,0 +1,123 @@
+import { randomUUID } from 'crypto';
+import { deleteFileFromS3, getPresignedUploadUrl } from './s3';
+import { Session } from 'next-auth';
+import { prisma } from './database';
+import { ForbiddenError, NotFoundError } from '@/utils/response';
+
+type GetSongFileUploadUrlProps = {
+  fileType?: string;
+  extension?: string;
+  type?: string;
+};
+
+export const getSongFileUploadUrl = async ({
+  fileType,
+  extension,
+  type,
+}: GetSongFileUploadUrlProps) => {
+  let key = `songs/${randomUUID()}.${extension ?? 'mp3'}`;
+  if (type !== 'audio') {
+    key = `images/${randomUUID()}.${extension ?? 'jpg'}`;
+  }
+
+  const presignedUrl = await getPresignedUploadUrl({
+    key: key,
+    contentType: fileType ?? (type !== 'audio' ? 'image/jpeg' : 'audio/mpeg'),
+  });
+
+  return presignedUrl;
+};
+
+type CreateSongProps = {
+  url: string;
+  title: string;
+  duration: number;
+  artist: string;
+  albumCover?: string | null;
+  session: Session;
+};
+
+export const createSong = async (createSongProps: CreateSongProps) => {
+  return await prisma.song.create({
+    data: {
+      url: createSongProps.url,
+      title: createSongProps.title,
+      duration: createSongProps.duration,
+      artist: createSongProps.artist,
+      albumCover: createSongProps.albumCover,
+      uploaderId: createSongProps.session.user.id,
+    },
+  });
+};
+
+type GetUpdateFileUploadUrlProps = {
+  id: string | undefined;
+  fileType: string;
+  extension: string;
+  session: Session;
+};
+
+export const getUpdateFileUploadUrl = async ({
+  id,
+  fileType,
+  extension,
+  session,
+}: GetUpdateFileUploadUrlProps) => {
+  const song = await prisma.song.findUnique({
+    where: {
+      id: id ?? '',
+    },
+  });
+  if (!song) {
+    throw new NotFoundError('Song not found');
+  }
+  if (song.uploaderId !== session.user.id) {
+    throw new ForbiddenError('You are not the owner of this song');
+  }
+  if (song.albumCover) {
+    await deleteFileFromS3({ url: song.albumCover });
+  }
+  return await getSongFileUploadUrl({
+    fileType,
+    extension,
+    type: 'image',
+  });
+};
+
+type UpdateSongProps = {
+  id: string | undefined;
+  title?: string | null;
+  artist?: string | null;
+  albumCover?: string | null;
+  session: Session;
+};
+
+export const updateSong = async ({
+  id,
+  title,
+  artist,
+  albumCover,
+  session,
+}: UpdateSongProps) => {
+  const song = await prisma.song.findUnique({
+    where: {
+      id: id ?? '',
+    },
+  });
+  if (!song) {
+    throw new NotFoundError('Song not found');
+  }
+  if (song.uploaderId !== session.user.id) {
+    throw new ForbiddenError('You are not the owner of this song');
+  }
+  return await prisma.song.update({
+    where: {
+      id: song.id,
+    },
+    data: {
+      title: title ?? song.title,
+      artist: artist ?? song.artist,
+      albumCover: albumCover ?? song.albumCover,
+    },
+  });
+};
