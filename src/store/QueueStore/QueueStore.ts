764e6a3e96422c6b08c3d633e6d29596f9b84764
Nasrullah <nasrullah01n@gmail.com>
2023-10-16 23:59:16 +0800

    Fixed shuffling bugs

--- src/store/QueueStore/QueueStore.ts
+++ src/store/QueueStore/QueueStore.ts
@@ -40,12 +40,14 @@ export const useQueueStore = create<QueueState & QueueActions>()(
       playNext: force => set(playNext(force)),
       playPrev: force => set(playPrev(force)),
       playPlaylist: (playlist, songId) => {
-        set(playPlaylist(playlist, songId));
-        get().setShuffle(get().shuffle);
+        set(playPlaylist(playlist, songId, () => {
+          get().setShuffle(get().shuffle);
+        }));
       },
       playSong: (song, songs) => {
-        set(playSong(song, songs));
-        get().setShuffle(get().shuffle);
+        set(playSong(song, songs, () => {
+          get().setShuffle(get().shuffle);
+        }));
       },
       setCurrentlyPlayingId: id => set(setCurrentlyPlayingId(id)),
       setCurrentTime: time => set(setCurrentTime(time)),
